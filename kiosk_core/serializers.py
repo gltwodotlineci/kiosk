d4ea8f9a881a386f9bad9911441fbe8c18ff3cc9
glen <glen84_dr@hotmail.com>
2024-05-04 15:01:24 +0200

    serializer

---
+++ kiosk_core/serializers.py
@@ -0,0 +1,23 @@
+from kiosk_core.models import Card
+from rest_framework import serializers
+
+# Validate the number card recived by the miniserver
+class CardValidator(serializers.Serializer):
+    number = serializers.CharField(required=True)
+
+    # check if the given card is right
+    def validate_number(self, value):
+        try:
+            Card.objects.get(number=value)
+            return value
+        except Card.DoesNotExist:
+            raise serializers.ValidationError("The card does not exist")
+
+
+# Serializer to create the data
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        fields = '__all__'
+
+
--- kiosk_core/views.py
+++ kiosk_core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from kiosk_core.serializers import CardValidator, CardSerializer
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from kiosk_core.models import Card
 
 
 def index(request):
@@ -13,6 +17,21 @@ def show(request):
 class CardViewset(viewsets.ViewSet):
     @action(detail=False, methods=['post'])
     def scan(self, request):
-        pass
+        if request.method == 'POST':
+            card_data = CardValidator(data=request.data)
+            # check if the scaned card doesn't exist or is unvalid
+            if not card_data.is_valid():
+                messages.add_message(request, messages.WARNING,
+                f"Wrong card, please try again")
+                return HttpResponseRedirect('/')
+
+            card_number = card_data.validated_data.get('number')
+
+            card_obj = Card.objects.get(number=card_number)
+            card_serialized = CardSerializer(Card, many=False)
+            card = card_serialized.data
+            return render(request,
+                'kiosk_pages/show.html',
+                {'card':card} )
 
 
